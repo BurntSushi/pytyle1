784bd3460dac65436847b66a41d4e7e3421ba617
andrew <andrew@Truth.localdomain>
2009-08-23 22:06:44 -0400

    Updated version number in setup.py.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
       name = "pytyle",
       author = "Andrew Gallant",
       author_email = "andrew@pytyle.com",
-      version = "0.5",
+      version = "0.6",
       license = "GPL",
       description = "A manual tiling manager for EWMH compliant window managers",
       long_description = "See README",
